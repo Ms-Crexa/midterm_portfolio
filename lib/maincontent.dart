e99c5cc1b53b9829c8bdcb9b990c2c071bac5589
Ninya <mnamparaiso@addu.edu.ph>
2024-10-02 17:06:03 +0800

    proper placing of image

--- lib/maincontent.dart
+++ lib/maincontent.dart
@@ -10,58 +10,62 @@ class Maincontent extends StatefulWidget {
 class _MaincontentState extends State<Maincontent> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      margin: EdgeInsets.only(top: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      margin: EdgeInsets.only(top: 100, left: 80, right: 150),
+      child: Stack(
         children: [
-          Container(
-            margin: EdgeInsets.only(left: 20),
-            child: Column(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'HE',
-                      style: TextStyle(
-                        fontSize: 90, // Bigger font size
-                        fontWeight: FontWeight.bold, // Bolder text
-                        color: Colors.blue, // Blue color
-                        shadows: [
-                          Shadow(
-                            blurRadius: 10.0,
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            offset: Offset(5.0, 5.0), // Shadow position
-                          ),
-                        ],
-                      ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'HE',
+                    style: TextStyle(
+                      fontSize: 160,
+                      height: 0.9,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black.withOpacity(0.5),
+                          offset: Offset(10.0, 10.0),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'LLO.',
-                      style: TextStyle(
-                        fontSize: 90, // Bigger font size
-                        fontWeight: FontWeight.bold, // Bolder text
-                        color: Colors.blue, // Blue color
-                        shadows: [
-                          Shadow(
-                            blurRadius: 10.0,
-                            color:
-                                Colors.black.withOpacity(0.5), // Shadow color
-                            offset: Offset(5.0, 5.0), // Shadow position
-                          ),
-                        ],
-                      ),
+                  ),
+                  Text(
+                    'LLO.',
+                    style: TextStyle(
+                      fontSize: 160,
+                      height: 0.9,
+                      fontWeight: FontWeight.bold,
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black.withOpacity(0.5),
+                          offset: Offset(5.0, 5.0),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            right: 0,
+            bottom: 0,
+            child: Image.network(
+              'https://placehold.co/600x800.png',
             ),
           ),
-          Image.network(
-              'https://d2bzx2vuetkzse.cloudfront.net/fit-in/0x450/unshoppable_producs/f7b9d7e3-5e5b-4b5c-802b-2a6882a25ea5.png')
         ],
       ),
     );
